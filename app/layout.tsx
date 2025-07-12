161fd1c0e2c774146da2d85b324395cd77a761dc
DongjeenHan <dongjeenhan@gmail.com>
2025-07-12 14:58:34 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,19 @@
+'use client';
 import './globals.css';
 import Header from '../components/header';
-
-export const metadata = {
-  title: 'Maple Leaf Animal Rescue',
-  description: 'A Home Until They Find Their Own',
-};
-
 import { ReactNode } from 'react';
+import { SessionProvider } from 'next-auth/react';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-gray-100 text-gray-900 min-h-screen">
-        <div className="min-h-screen flex flex-col">
-          <Header /> {/* Only can see when logged in */}
-          <main className="flex-1 max-w-4xl mx-auto p-4">{children}</main>
-        </div>
+        <SessionProvider>
+          <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1 max-w-4xl mx-auto p-4">{children}</main>
+          </div>
+        </SessionProvider>
       </body>
     </html>
   );

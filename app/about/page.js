1067b30507d92cec45466da464b8969d3e3700ba
Jiwon <Jiwon.Uh@edu.sait.ca>
2025-07-13 22:02:57 -0600

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -16,8 +16,8 @@ export default function AdminDashboardPage() {
         <h1 className="font-stretch-expanded tracking-wide">
             Welcome to Maple leaf Animal Rescue, where hearts meet paws and second chances begin.
         </h1>
-        <p className="mt-9 text-base">We’re a passionate team of animal lovers, rescuers, and volunteers who believe every animal deserves a safe, loving home. Since our founding in 2025, we've dedicated ourselves to rescuing, rehabilitating, and rehoming abandoned, abused, and homeless animals across Calgary,Canada.</p>
-        <p className="mt-4 text-base mb-10">At Maple Leaf Animal Rescue, we work closely with local shelters, foster families, and veterinarians to ensure every animal gets the care, attention, and love they need. Whether it’s a playful puppy, a gentle senior cat, or a misunderstood mutt ~ we believe each one has a story worth hearing and a future worth fighting for.</p>
+        <p className="mt-9 text-base">we&apos;re a passionate team of animal lovers, rescuers, and volunteers who believe every animal deserves a safe, loving home. Since our founding in 2025, we&apos;ve dedicated ourselves to rescuing, rehabilitating, and rehoming abandoned, abused, and homeless animals across Calgary,Canada.</p>
+        <p className="mt-4 text-base mb-10">At Maple Leaf Animal Rescue, we work closely with local shelters, foster families, and veterinarians to ensure every animal gets the care, attention, and love they need. Whether it&apos;s a playful puppy, a gentle senior cat, or a misunderstood mutt ~ we believe each one has a story worth hearing and a future worth fighting for.</p>
       </div>
 
       {/* Three main missions */}
@@ -33,13 +33,13 @@ export default function AdminDashboardPage() {
       {/* More explainations */}
       <div className="mt-10 border-t-4 text-base text-red-900">
         <p className="mt-10">We also believe in building a community. Through adoption events, educational programs, and volunteer opportunities, we aim to connect people with animals — and with each other.</p>
-        <p className="mt-4">By adopting through Maple Leaf Animal Rescue, you're not just saving one life. You're making space for another. You're becoming a hero in the eyes of an animal who only wants to love and be loved.</p>
+        <p className="mt-4">By adopting through Maple Leaf Animal Rescue, you&apos;re not just saving one life. You&apos;re making space for another. You&apos;re becoming a hero in the eyes of an animal who only wants to love and be loved.</p>
       </div>
 
       {/* Call to action */}
       <div className="text-center mt-10 border-t-3 text-red-900">
         <h1 className="mt-9">Ready to find your new best friend? 🐶🐱</h1>
-        <h1 className="mt-2">Let’s change lives together — one paw at a time.</h1>
+        <h1 className="mt-2">Let&apos;s change lives together — one paw at a time.</h1>
       </div>
     </div>
   );
